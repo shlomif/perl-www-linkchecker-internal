2b4baef9af201c37170c3e2244333491cee8002a
Shlomi Fish <shlomif@shlomifish.org>
2012-08-30 00:48:58 +0300

    Bump the version number to 0.0.3.

--- WWW-LinkChecker-Internal/lib/WWW/LinkChecker/Internal.pm
+++ WWW-LinkChecker-Internal/lib/WWW/LinkChecker/Internal.pm
@@ -10,11 +10,11 @@ WWW::LinkChecker::Internal - Check a web site for broken internal links.
 
 =head1 VERSION
 
-Version 0.0.2
+Version 0.0.3
 
 =cut
 
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 =head1 SYNOPSIS
 
