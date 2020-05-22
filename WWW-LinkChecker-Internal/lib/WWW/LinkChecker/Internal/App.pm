b72c3efb43c73eeb0fbaf6d104ea8940e45884ca
Shlomi Fish <shlomif@shlomifish.org>
2020-05-22 17:56:29 +0300

    Extract a common class/struct.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://en.wikipedia.org/wiki/God_object
    
    * https://en.wikipedia.org/wiki/Extract_class
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

---
+++ WWW-LinkChecker-Internal/lib/WWW/LinkChecker/Internal/App.pm
@@ -0,0 +1,8 @@
+package WWW::LinkChecker::Internal::App;
+
+use strict;
+use warnings;
+
+use App::Cmd::Setup -app;
+
+1;
---
+++ WWW-LinkChecker-Internal/lib/WWW/LinkChecker/Internal/App/Command/check.pm
@@ -0,0 +1,97 @@
+package WWW::LinkChecker::Internal::App::Command::check;
+
+use strict;
+use warnings;
+use 5.014;
+
+use WWW::LinkChecker::Internal::App -command;
+
+sub description
+{
+    return "check a site for broken internal links";
+}
+
+sub abstract
+{
+    return shift->description();
+}
+
+sub opt_spec
+{
+    return (
+        [ "base=s",                "Base URL", ],
+        [ 'before-insert-skip=s@', "before-insert-skip regexes", ],
+        [ 'pre-skip=s@',           "pre-skip regexes", ],
+        [ 'start=s',               "alternative start URL", ],
+        [ 'state-filename=s' => 'filename to keep the state', ],
+    );
+}
+
+sub execute
+{
+    my ( $self, $opt, $args ) = @_;
+    my $base_url = $opt->{base_url};
+    if ( !defined($base_url) )
+    {
+        die "--base must be specified";
+    }
+
+    my @pre_skip_regexes = map { qr/$_/ } @{ $opt->{pre_skip_texts} };
+    my @before_insert_skips_regexes =
+        map { qr/$_/ } @{ $opt->{before_insert_skips_texts} };
+
+    my $alternative_start_url = $opt->{start};
+    my $state_fn              = $opt->{state_filename};
+    my $start_url             = ( $alternative_start_url || $base_url );
+
+    my $state =
+        +( $state_fn && ( -e $state_fn ) )
+        ? decode_json( path($state_fn)->slurp_utf8 )
+        : {
+        stack            => [ { url => $start_url, from => undef(), } ],
+        encountered_urls => { $start_url => 1, },
+        };
+STACK:
+
+    while ( my $url_rec = $state->{stack}->[-1] )
+    {
+        my $url = $url_rec->{'url'};
+        print "Checking SRC URL '$url'\n";
+
+        my $mech = WWW::Mechanize->new();
+        eval { $mech->get($url); };
+
+        if ($@)
+        {
+            if ($state_fn)
+            {
+                path($state_fn)->spew_utf8( encode_json($state) );
+            }
+            die "SRC URL $url_rec->{from} points to '$url'.";
+        }
+        pop( @{ $state->{stack} } );
+
+        if ( any { $url =~ $_ } @pre_skip_regexes )
+        {
+            next STACK;
+        }
+
+        foreach my $link ( $mech->links() )
+        {
+            my $dest_url = $link->url_abs() . "";
+            $dest_url =~ s{#[^#]+\z}{}ms;
+            if (    ( !exists( $state->{encountered_urls}->{$dest_url} ) )
+                and $dest_url =~ m{\A\Q$base_url\E}ms
+                and ( none { $dest_url =~ $_ } @before_insert_skips_regexes ) )
+            {
+                $state->{encountered_urls}->{$dest_url} = 1;
+                push @{ $state->{stack} }, { url => $dest_url, from => $url, };
+            }
+        }
+    }
+
+    print
+"Finished checking the site under the base URL '$base_url'.\nNo broken links were found\n";
+
+    return;
+}
